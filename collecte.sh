7918f6110d33c89f6ed4c8cba34529a51ced0aa3
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-22 09:04:15 -0700

    Update collecte.sh

--- collecte.sh
+++ collecte.sh
@@ -1,2 +1,17 @@
 #!/bin/bash -ex
-#import json
+printf timesatmp, temperature, power1, power2, total_power >> collected.csv
+export x=$(sudo eri-ipmitool rns 0)
+time=${x:54:24}
+temp=${x:170:8}
+export x=$(sudo eri-ipmitool rns 4)
+volt1=${x:170:8}
+export x=$(sudo eri-ipmitool rns 5)
+volt2=${x:170:8}
+export x=$(sudo eri-ipmitool rns 68)
+crt1=${x:170:8}
+export x=$(sudo eri-ipmitool rns 69)
+crt2=${x:170:8}
+power1=$(calc $volt1*$crt1)
+power2=$(calc $volt2*$crt2)
+total_power==$(calc $power1+$power2)
+printf $time, $temp, $power1, $power2, $total_power >> collected.csv
