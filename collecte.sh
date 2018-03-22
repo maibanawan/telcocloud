680eb0c9470b5e3387826da98ff3e5d69575efde
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-22 09:10:08 -0700

    Update collecte.sh

--- collecte.sh
+++ collecte.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-printf timesatmp, temperature, power1, power2, total_power >> collected.csv
+printf timesatmp,temperature,power1,power2,total_power >> collected.csv
 export x=$(sudo eri-ipmitool rns 0)
 time=${x:54:24}
 temp=${x:170:8}
@@ -14,4 +14,4 @@ crt2=${x:170:8}
 power1=$(calc $volt1*$crt1)
 power2=$(calc $volt2*$crt2)
 total_power==$(calc $power1+$power2)
-printf $time, $temp, $power1, $power2, $total_power >> collected.csv
+printf $time,$temp,$power1,$power2,$total_power >> collected.csv
