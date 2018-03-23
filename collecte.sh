a19d39c4b4d3926f8eaf5e1bf9224e76d70c32a5
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-23 12:39:15 -0700

    Update collecte.sh

--- collecte.sh
+++ collecte.sh
@@ -18,5 +18,5 @@ do
    total_power=$(calc $power1+$power2)
    echo $time,$temp,$power1,$power2,$total_power >> collected000.csv
    awk -F"," '{printf "%-30s%-20s%-20s%-20s%-20s\n",$1,$2,$3,$4,$5}' collected000.csv > collected1000.csv
-   sleep 0.5
+   sleep 0.25
 done
