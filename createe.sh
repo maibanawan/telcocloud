e219036f6161394b6d970238a2d6550f8e57c337
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-24 16:07:18 -0700

    Update createe.sh

--- createe.sh
+++ createe.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
-echo $count
 N=$(( 3 + $RANDOM % 3 ))
 echo $N
 for i in $(seq 1 $N);
 do 
+echo vm_$i_$count
 openstack server create --nic net-id=a9d9c7ec-b335-485c-8b3a-a68ee9079537 --flavor tempest2 --image 75d4f34f-ee34-4cd2-815b-c835b0132ae3 vm_$i_$count
 done
