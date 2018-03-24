c360d072ef22dd74a2e1763bde51b1e666c1d549
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-24 12:45:57 -0700

    Update arrivale.sh

--- arrivale.sh
+++ arrivale.sh
@@ -1,7 +1,10 @@
 #!/bin/env bash
 
-end=$((SECONDS+3))
+end=$((SECONDS+5m))
 
 while [ $SECONDS -lt $end ]; do
-   echo $SECONDS
+   interval=$(( 1 + $RANDOM % 5 ))
+   interval_sec=$(calc $interval*60)
+   source createe.sh
+   sleep $interval_sec
 done
