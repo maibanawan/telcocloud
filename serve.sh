41615a8d6ec35b72a7f46a03e16b70b980484e82
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-24 15:38:44 -0700

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -7,7 +7,9 @@ lenx=${#x}
 y=$(calc $lenx+1)
 num_vms=$(calc $y/5)
 echo $num_vms
-
+Num=$num_vms
+for i in in $(seq 1 $Num);
+do 
 #select name of vm randomly
 vm=$(( 1 + $RANDOM % $num_vms ))
 echo $vm
@@ -16,3 +18,10 @@ echo $start
 vm_name=${x:start:4}
 openstack server delete $vm_name
 
+openstack server list | awk '{print $4}' | grep -v ^N | grep -v ^$ | paste -sd ","> list.csv
+x=$(cat list.csv | awk '{print $1}')
+lenx=${#x}
+y=$(calc $lenx+1)
+num_vms=$(calc $y/5)
+echo $num_vms
+done
