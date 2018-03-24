07890026b35ca4c734a230633a190fd508fdf917
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-24 16:34:44 -0700

    Update arrivale.sh

--- arrivale.sh
+++ arrivale.sh
@@ -3,11 +3,11 @@
 end=$((SECONDS+600))
 count=1
 while [ $SECONDS -lt $end ]; do
-   interval=$(( 1 + $RANDOM % 5 ))
+   interval=$(( 3 + $RANDOM % 3 ))
    echo $interval
    interval_sec=$(calc $interval*60)
    source createe.sh
-   #sleep $interval_sec
+   sleep $interval_sec
    c=$(( $count + 1 ))
    count=$c 
 done
