a8d75e3ad27d32b6d82cf23315b0cddee8ac0649
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-24 14:45:32 -0700

    Create serve.sh

---
+++ serve.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#geting names of instances to delete
+openstack server list | awk '{print $4}' | grep -v ^N | grep -v ^$ | paste -sd ","> list.csv
+x=$(cat list.csv | awk '{print $1}')
+lenx=${#x}
+y=$(calc $lenx+1)
+num_vms=$(calc $y/5)
+echo $num_vms
+
+#select name of vm randomly
+vm=$(( 1 + $RANDOM % $num_vms ))
+start=$(calc $(calc $v-1)*5)
+vm_name=${x:start:4}
+openstack server delete $vm_name
+
