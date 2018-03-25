ab7e59f0116bfd311b24e5aa4a121255c13b21d8
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-25 00:01:15 -0700

    Update adapte.sh

--- adapte.sh
+++ adapte.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 up_threshold=80
 down_threshold=20
+
 fmax=1996000
 export available_f=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies)
 #while true
@@ -14,32 +15,37 @@ export available_f=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_
  export utilization=$(calc 100 - $idle)
  if (( $utilization > $up_threshold ))
  then
-  for j in $(seq 1 4)
-  do
-   index=$(( 5 - $j ))
-   if (( $current_f == ${available_f[$index]} ))
-   then
-    next_index=$(( index - 1 ))
-    next_freq=${available_f[$next_index]}
-   fi
-  done
+  if (( $current_f == 1596000 ))
+  then
+   next_freq=1729000
+  elif (( $current_f == 1729000 )) 
+  then
+   next_freq=1862000
+  elif (( $current_f == 1862000 )) 
+  then
+   next_freq=1995000 
+  elif (( $current_f == 1995000 )) 
+  then
+   next_freq=1996000 
+  fi
   sudo sh -c "echo -n "userspace" > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor"
   sudo sh -c "echo -n  $next_freq > /sys/devices/system/cpu/cpuX/cpufreq/scaling_setspeed"
   echo $current_f $next_freq
  elif (( $utilization < $down_threshold ))
  then   
-  for j in $(seq 1 3)
-  do
-   index=$j
-   if (( $current_f == ${available_f[$index]} ))
-   then
-    next_index=$(( index + 1 ))
-    next_freq=${available_f[$next_index]}
-   elif (( $current_f == $fmax ))
-   then
-    next_freq=${available_f[1]}
-   fi
-  done
+  if (( $current_f == 1996000 ))
+  then
+   next_freq=1995000
+  elif (( $current_f == 1995000 )) 
+  then
+   next_freq=1862000
+  elif (( $current_f == 1862000 )) 
+  then
+   next_freq=1729000 
+  elif (( $current_f == 1729000 )) 
+  then
+   next_freq=1596000 
+  fi
   sudo sh -c "echo -n "userspace" > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor"
   sudo sh -c "echo -n  $next_freq > /sys/devices/system/cpu/cpuX/cpufreq/scaling_setspeed"
   echo $current_f $next_freq
