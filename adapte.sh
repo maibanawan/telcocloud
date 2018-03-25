85bdd95e6be5993f0c13d6d98cb4529d274dcd69
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-24 22:35:28 -0700

    Create adapte.sh

---
+++ adapte.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+up_threshold=80
+down_threshold=20
+fmax=1996000
+while true
+do
+ for i in in $(seq 1 11);
+ do
+ export available_f=$(cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_available_frequencies)
+ export current_f=$(cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq)
+ export x=$(mpstat -P $i | awk '{print $13}')
+ export idle=${x:8:5}
+ export utilization=$(calc 100 - $idle)
+ echo $utilization
+ done
+done
