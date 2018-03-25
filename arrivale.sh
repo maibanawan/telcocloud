b08ab027bfa49bbdec0427b3294e44d3f4a8c35d
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-25 05:27:18 -0700

    Update arrivale.sh

--- arrivale.sh
+++ arrivale.sh
@@ -2,6 +2,8 @@
 
 end=$((SECONDS+300))
 count=1
+total_time=0
+total_vms=0
 while [ $SECONDS -lt $end ]; do
    interval=$(( 3 + $RANDOM % 3 ))
    echo $interval
@@ -10,4 +12,8 @@ while [ $SECONDS -lt $end ]; do
    #sleep $interval_sec
    c=$(( $count + 1 ))
    count=$c 
+   total_time=$(( $total_time + $interval ))
+   total_vms=$(( $total_vms + $N ))
 done
+echo total_time $total_time
+echo total_vms $total_vms
