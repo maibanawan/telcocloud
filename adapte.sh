dcecf6ca07d96f0a2a8be219868675706311a96b
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-24 23:14:29 -0700

    Update adapte.sh

--- adapte.sh
+++ adapte.sh
@@ -12,6 +12,37 @@ export available_f=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_
  export x=$(mpstat -P $i | awk '{print $13}')
  export idle=${x:8:5}
  export utilization=$(calc 100 - $idle)
- echo $utilization
+ if (( $utilization > $up_threshold ))
+ then
+  for j in $(seq 1 4)
+  do
+   index=$(( 5 - $j ))
+   if (( $current_f == ${available_f[$index]} ))
+   then
+    next_index=$(( index - 1 ))
+    next_freq=${available_f[$next_index]}
+   fi
+  done
+  sudo sh -c "echo -n "userspace" > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor"
+  sudo sh -c "echo -n  $next_freq > /sys/devices/system/cpu/cpuX/cpufreq/scaling_setspeed"
+  echo $current_f $next_freq
+ elif (( $utilization < $down_threshold ))
+ then   
+  for j in $(seq 1 3)
+  do
+   index=$j
+   if (( $current_f == ${available_f[$index]} ))
+   then
+    next_index=$(( index + 1 ))
+    next_freq=${available_f[$next_index]}
+   elif (( $current_f == $fmax ))
+   then
+    next_freq=${available_f[1]}
+   fi
+  done
+  sudo sh -c "echo -n "userspace" > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor"
+  sudo sh -c "echo -n  $next_freq > /sys/devices/system/cpu/cpuX/cpufreq/scaling_setspeed"
+  echo $current_f $next_freq
+ fi
  done
 #done
