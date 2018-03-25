3f76a5e72fec780230f4f003a73449902d79d705
mb <37088884+maibanawan@users.noreply.github.com>
2018-03-25 00:05:52 -0700

    Update adapte.sh

--- adapte.sh
+++ adapte.sh
@@ -6,7 +6,7 @@ fmax=1996000
 export available_f=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies)
 #while true
 #do
- for i in $(seq 1 11);
+ for i in $(seq 0 11);
  do
  #export available_f=$(cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_available_frequencies)
  export current_f=$(cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq)
